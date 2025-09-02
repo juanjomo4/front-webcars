908a26ca365c97d85c34a80b42eddbe06c9d72a3
Juanjo M. <60217633+juanjomo4@users.noreply.github.com>
2025-09-02 13:14:23 +0200

    Pruebas con el card y el responsive de la página.

--- src/app/shared/pages/home-page/home-page.component.ts
+++ src/app/shared/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { CardComponent } from "../../components/card/card.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, CardComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
